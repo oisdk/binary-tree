1108675e0ee45e9312de13d59887360248791a9e
Donnacha Oisín Kidney <mail@doisinkidney.com>
2018-02-09 14:42:05 +0000

    new drawing algorithm

--- src/Data/Tree/Binary/Internal.hs
+++ src/Data/Tree/Binary/Internal.hs
@@ -20,77 +20,64 @@ import Data.Functor.Identity (Identity(..))
 
 import Data.Functor (Functor(fmap))
 import Control.Applicative (Applicative((<*>), pure))
-import Data.Monoid (Monoid(mappend, mempty))
+
+--------------------------------------------------------------------------------
+-- Drawing Trees
+--------------------------------------------------------------------------------
+data Padding =
+  Padding {-# UNPACK #-}!Int
+          Prefix
+
+data Prefix
+  = Nil
+  | Prefix String
+           Padding
 
 data LevelBuilder = LevelBuilder
-  { _offset :: {-# UNPACK #-} !Int
-  , levels :: [Level]
+  { _offset :: {-# UNPACK #-}!Int
+  , levels :: [Padding -> Padding]
   }
-
-data Level
-  = Nil
-  | Padding {-# UNPACK #-} !Int Level
-  | Prefix (String -> String) Level
-
-runLevel :: Level -> String -> String
-runLevel Nil st = st
-runLevel (Padding p xs) st = replicate p ' ' ++ runLevel xs st
-runLevel (Prefix p xs) st = p (runLevel xs st)
-
-instance Monoid Level where
-  mempty = Nil
-  mappend (Padding x Nil) (Padding y ys) = Padding (x + y) ys
-  mappend (Prefix x Nil) (Prefix y ys) = Prefix (x . y) ys
-  mappend (Padding x xs) ys = Padding x (mappend xs ys)
-  mappend (Prefix x xs) ys = Prefix x (mappend xs ys)
-  mappend Nil ys = ys
-
-padding :: Int -> Level -> Level
-padding 0 ys = ys
-padding n (Padding y ys) = Padding (n+y) ys
-padding n ys = Padding n ys
 
 drawBinaryTree ::
      Show a
   => (LevelBuilder -> (a -> LevelBuilder -> LevelBuilder -> LevelBuilder) -> b -> LevelBuilder)
   -> b
   -> String
-drawBinaryTree ft = unlines . levels . ft (LevelBuilder 0 []) f
+drawBinaryTree ft = runLevels . levels . ft (LevelBuilder 0 []) f
   where
-    f el (LevelBuilder llen lb) (LevelBuilder rlen rb) =
+    f xv (LevelBuilder llen lb) (LevelBuilder rlen rb) =
       LevelBuilder
         (llen + rlen + xlen)
-        (padding llen (Prefix xshw (padding rlen Nil)) : zipLongest lb rb)
+        ((Padding llen . Prefix xshw . cons rlen) : zipLongest lb rb)
       where
-        xshw = shows el
-        xlen = length (xshw "")
-        zipLongest (x:xs) (y:ys) = comb3 x xlen y : zipLongest xs ys
-        zipLongest [] ys = lfn ys
-        zipLongest xs [] = rfn xs
-        rfn
-          | xr == 0 = id
-          | otherwise = map go
-          where
-            xr = xlen + rlen
-            go (Padding x Nil) = Padding (x + xr) Nil
-            go (Padding x xs) = Padding x (go xs)
-            go (Prefix x xs) = Prefix x (go xs)
-            go Nil = Padding xr Nil
-        lfn
-          | xl == 0 = id
-          | otherwise = map go
-          where
-            xl = llen + xlen
-            go (Padding x xs) = Padding (xl + x) xs
-            go xs = Padding xl xs
-        comb3 xs 0 ys = mappend xs ys
-        comb3 Nil l ys = padding l ys
-        comb3 (Padding x Nil) l ys = padding (x + l) ys
-        comb3 (Padding x xs) l ys = Padding x (comb3 xs l ys)
-        comb3 (Prefix x xs) l ys = Prefix x (comb3 xs l ys)
-    unlines [] = ""
-    unlines (x:xs) = runLevel x (foldr (\e a -> '\n' : runLevel e a) "" xs)
+        xshw = show xv
+        xlen = length xshw
+        zipLongest (x:xs) (y:ys) = (x . cons xlen . y) : zipLongest xs ys
+        zipLongest [] ys = map (cons (llen + xlen) .) ys
+        zipLongest xs [] = map (. cons (rlen + xlen)) xs
 
+cons :: Int -> Padding -> Padding
+cons i (Padding j xs) = Padding (i + j) xs
+
+nil :: Padding
+nil = Padding 0 Nil
+
+runLevels :: [Padding -> Padding] -> String
+runLevels [] = ""
+runLevels (x:xs) =
+  runLevel (x nil) (foldr (\e a -> '\n' : runLevel (e nil) a) "" xs)
+
+runLevel :: Padding -> String -> String
+runLevel (Padding n xs) = pad n . f xs
+  where
+    pad 0 = id
+    pad m = (' ' :) . pad (m - 1)
+    f Nil = id
+    f (Prefix s ys) = (s ++) . runLevel ys
+
+--------------------------------------------------------------------------------
+-- State
+--------------------------------------------------------------------------------
 newtype State s a = State
   { runState :: s -> (a, s)
   }
@@ -101,9 +88,11 @@ instance Functor (State s) where
       (\s ->
          case runState xs s of
            (x, s') -> (f x, s'))
+  {-# INLINE fmap #-}
 
 instance Applicative (State s) where
   pure x = State (\s -> (x, s))
+  {-# INLINE pure #-}
   fs <*> xs =
     State
       (\s ->
@@ -111,10 +100,15 @@ instance Applicative (State s) where
            (f, s') ->
              case runState xs s' of
                (x, s'') -> (f x, s''))
+  {-# INLINE (<*>) #-}
 
 evalState :: State s a -> s -> a
 evalState xs s = fst (runState xs s)
+{-# INLINE evalState #-}
 
+--------------------------------------------------------------------------------
+-- Identity
+--------------------------------------------------------------------------------
 #if !MIN_VERSION_base(4,8,0)
 newtype Identity a = Identity {runIdentity :: a}
 
